d37ca1b22dda6ec1394daab5a2f9058fde288c70
FukeKazki <kazkichi0906@gmail.com>
2022-05-12 23:05:20 +0900

    Update: roomでも動作するように修正

--- functions/src/handler/lineHandler/messageHandler.ts
+++ functions/src/handler/lineHandler/messageHandler.ts
@@ -8,23 +8,41 @@ export const messagesHandler = async (event: MessageEvent): Promise<void> => {
   switch (event.message.type) {
     case "text":
       if (event.message.type !== "text") return;
-      if (event.source.type !== "group") return;
+      if (event.source.type === "group") {
+        const { groupId, userId } = event.source;
+        if (!userId) return;
 
-      const { groupId, userId } = event.source;
-      if (!userId) return;
+        const text = event.message.text;
+        const replyToken = event.replyToken;
 
-      const text = event.message.text;
-      const replyToken = event.replyToken;
+        const roomRepository = new RoomRepository(app);
+        const roomService = new RoomService(roomRepository);
 
-      const roomRepository = new RoomRepository(app);
-      const roomService = new RoomService(roomRepository);
+        const replyText = await textUseCase(text, userId, groupId, roomService);
+        if (!replyText) return;
+        lineClient.replyMessage(replyToken, {
+          type: "text",
+          text: replyText
+        });
+      }
 
-      const replyText = await textUseCase(text, userId, groupId, roomService);
-      if (!replyText) return;
-      lineClient.replyMessage(replyToken, {
-        type: "text",
-        text: replyText
-      });
+      if (event.source.type === "room") {
+        const { roomId, userId } = event.source;
+        if (!userId) return;
+
+        const text = event.message.text;
+        const replyToken = event.replyToken;
+
+        const roomRepository = new RoomRepository(app);
+        const roomService = new RoomService(roomRepository);
+
+        const replyText = await textUseCase(text, userId, roomId, roomService);
+        if (!replyText) return;
+        lineClient.replyMessage(replyToken, {
+          type: "text",
+          text: replyText
+        });
+      }
     default:
       return;
   }
